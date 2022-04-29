625a3fac6b422734efdcd21b9d71a3bab8d7e7ec
estebanC-302 <estebanrcc69@gmail.com>
2022-04-29 07:46:56 -0300

    modificacion de datos

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      contador: 0
+    };
+  }
+
+  aumentarContador = () => {
+    this.setState({
+      contador: this.state.contador + 1
+    });
+  };
+
+  diminuirContador = () => {
+    this.setState({
+      contador: this.state.contador - 1
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Contador {this.state.contador}</h1>
+        <button onClick={this.aumentarContador}>+ Sumar</button>
+        <button onClick={this.diminuirContador}>- Restar</button>
+
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
+
+
